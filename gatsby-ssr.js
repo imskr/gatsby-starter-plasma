82026450c6cec261f910fc4878295ae5ac91ee37
Shubham Kumar <Shubhamkrai123@gmail.com>
2020-06-13 14:04:53 +0530

    add Buy me a Coffee widget

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,16 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
-
-// You can delete this file if you're not using it
+const React = require("react");
+exports.onRenderBody = ({ setPostBodyComponents }) => {
+  setPostBodyComponents([
+    <script
+      data-name="BMC-Widget"
+      src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+      data-id="imskr"
+      data-description="Support me on Buy me a coffee!"
+      data-message="Thank you for visiting. You can now buy me a coffee!"
+      data-color="#79D6B5"
+      data-position="right"
+      data-x_margin="28"
+      data-y_margin="18"
+    ></script>
+  ]);
+};
