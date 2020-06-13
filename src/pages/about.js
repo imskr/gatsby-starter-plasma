5a25b0f93acbb6564468293836d75305e393838c
Shubham Kumar <Shubhamkrai123@gmail.com>
2020-06-13 20:28:51 +0530

    add about.js

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../components/layout"
+const AboutPage = () => (
+  <main>
+  	<Layout>
+    <h1>About the Author</h1>
+    <p>Welcome to my Gatsby site.</p>
+    </Layout>
+  </main>
+)
+export default AboutPage
