32009dd05f5d5835d8e44e4488a9f0ffbb4693bd
Shubham Kumar <Shubhamkrai123@gmail.com>
2020-06-17 07:20:24 +0530

    test header

--- src/components/header.js
+++ src/components/header.js
@@ -5,14 +5,13 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#f92672`,
       marginBottom: `1.45rem`,
     }}
   >
     <div
       style={{
         margin: `0 auto`,
-        maxWidth: 960,
         padding: `1.45rem 1.0875rem`,
       }}
     >
